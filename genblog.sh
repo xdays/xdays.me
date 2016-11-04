02418a7b072685d2c4a70dac253d37b687b1eac3
xdays <easedays@gmail.com>
2016-11-04 08:07:13 +0000

    update genblog.sh

--- genblog.sh
+++ genblog.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
  
-export LANG=zh_CN.UTF-8 && cd /web/blog && pelican -s pelicanconf.py content/ -t theme/ 
+export LANG=zh_CN.UTF-8 && pelican -s pelicanconf.py content/ -t theme/ 
