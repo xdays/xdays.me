055567baff8a1bdfa1ce6414fb9840613ce3bdd0
xdays <easedays@gmail.com>
2017-07-30 15:36:04 +0000

    fix wrong theme repo

--- genblog.sh
+++ genblog.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
  
 THEME_PATH=/tmp/attila
-git clone https://github.com/zutrinken/attila.git $THEME_PATH
+git clone https://github.com/arulrajnet/attila.git $THEME_PATH
 pelican-themes -U $THEME_PATH
 export LANG=zh_CN.UTF-8 && pelican content/ $@
 rm -rf $THEME_PATH
