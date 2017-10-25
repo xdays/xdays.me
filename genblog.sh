65127f521a26d2d67de5f65000e93fff40abffd9
xdays <easedays@gmail.com>
2017-10-25 23:00:35 +0800

    rollback to old theme

--- genblog.sh
+++ genblog.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
  
-THEME_PATH=/tmp/attila
-git clone https://github.com/arulrajnet/attila.git $THEME_PATH
-pelican-themes -U $THEME_PATH
-export LANG=zh_CN.UTF-8 && pelican content/ $@
-rm -rf $THEME_PATH
+export LANG=zh_CN.UTF-8 && pelican -s pelicanconf.py content/ -t theme/ $1
