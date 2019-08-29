52d2f0817a1b70b34d02207ca253fabc639366cf
xdays <easedays@gmail.com>
2019-08-29 16:30:16 +0800

    change rss icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `static/favicon.ico`,
       },
     },
     `gatsby-plugin-offline`,
